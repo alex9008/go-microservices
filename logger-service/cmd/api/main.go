a519d673cc9a83e35cbecf86886356b2e2c17cf4
aghdez <agarcia@dlocal.com>
2023-05-01 17:44:12 -0300

    add log-service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log-service/data"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	grpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+	Models data.Models
+}
+
+func main() {
+
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+
+	// create a context in order to disconect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// disconnect from mongo
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	// start the web server
+	//go app.startWebServer()
+
+	log.Println("Starting web server on port", webPort)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Panic()
+	}
+}
+
+func (app *Config) startWebServer() {
+	log.Println("Starting web server on port", webPort)
+	srv := &http.Server{
+		Addr:    ":" + webPort,
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic()
+	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect to mongo
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting", err)
+		return nil, err
+	}
+
+	log.Println("Connectd to mongo!")
+
+	return client, nil
+}
